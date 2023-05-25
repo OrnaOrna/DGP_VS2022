428fd6e8c6f96e5b5410e8f813e2c97483b841c1
Yair Oren <yair.oren@live.biu.ac.il>
2023-05-25 15:45:25 +0300

    Lots of refactorings

--- Commands/harmonicFlatteningCmd.cpp
+++ Commands/harmonicFlatteningCmd.cpp
@@ -7,6 +7,202 @@
 #include "Utils/MatlabGMMDataExchange.h"
 #include "Utils/MatlabInterface.h"
 
+// Calculates the (u,v) coordinates of the boundary vertices by chord length.
+unsigned int calculateBoundaryCoords(const MFnMesh& meshFn, MFloatArray& u, MFloatArray& v) {
+	std::list<int> boundary;
+	getBoundary(meshFn, boundary);
+
+	// Get the differences between the coords of pairs of vertices on the boundary
+	std::list<MPoint> boundaryCoords = {};
+	MPoint coords;
+	for (int vertex : boundary) {
+		meshFn.getPoint(vertex, coords);
+		boundaryCoords.emplace_back(coords);
+	}
+	std::list<MVector> differences = {};
+	std::adjacent_difference(boundaryCoords.begin(), boundaryCoords.end(), 
+							std::back_inserter(differences));
+
+
+	// Sum the lengths of the differences to get the total boundary length
+	double totalBoundaryLength = 0;
+	auto i = differences.begin();
+	for (++i; i != differences.end(); ++i) {
+		totalBoundaryLength += (*i).length();
+	}
+
+	// Initialize iterators to go over the boundaries except the first and last
+	auto j = boundary.begin(), j_end = boundary.end();
+	auto i_end = differences.end();
+	--j_end;
+	--i_end;
+	i = differences.begin();
+
+	// Set the boundary coords, at last
+	u[*j] = 1;
+	v[*j] = 0;
+	double cumulativeBoundaryLength = 0; 
+	for (++i, ++j ; i != i_end && j != j_end; ++i, ++j) {
+		cumulativeBoundaryLength += (*i).length() / totalBoundaryLength;
+		u[*j] = cos(2 * M_PI * cumulativeBoundaryLength);
+		v[*j] = sin(2 * M_PI * cumulativeBoundaryLength);
+	}
+
+	return meshFn.numVertices() - boundary.size() + 1;
+}
+
+// Initialize a map between rows in coordinate matrix and indexes in the mesh,
+// and the other way around
+void createMaps(const MFnMesh& meshFn, MIntArray& rowMap, 
+				std::map<int,int>& indexMap, unsigned int rowCount) {
+	MItMeshVertex vertex_it(meshFn.object());
+	int row = 0;
+	while (!vertex_it.isDone()) {
+		if (!vertex_it.onBoundary()) {
+			rowMap[row] = vertex_it.index();
+			indexMap[vertex_it.index()] = row;
+			++row;
+		}
+		vertex_it.next();
+	}
+}
+
+
+// Creates (u,v) coordinates for all vertices using a uniform-weight matrix,
+// Assuming the coordinates of the boundary are fixed and a row map is known
+void uniformWeights(const MFnMesh& meshFn, MFloatArray& u, MFloatArray& v, 
+					const MIntArray& rowMap, std::map<int,int>& indexMap, 
+					unsigned int rowCount) {
+	GMMSparseRowMatrix weight_matrix(rowCount, rowCount);
+	GMMDenseColMatrix rhs(rowCount, 2);
+
+	MItMeshVertex vertex_it(meshFn.object());
+
+	// Fill in the weight matrix and the rhs vector, for now with uniform weights
+	MIntArray connectedVertices;
+	for (unsigned int currRow = 0; currRow < rowCount; ++currRow) {
+		int _;
+		int rowSum = 0;
+		vertex_it.setIndex(rowMap[currRow], _);
+		vertex_it.getConnectedVertices(connectedVertices);
+		for (const int vertex : connectedVertices) {
+			vertex_it.setIndex(vertex, _);
+			++rowSum;
+
+			if (vertex_it.onBoundary()) {
+				rhs(currRow, 0) = - u[vertex];
+				rhs(currRow, 1) = - v[vertex];
+			} else {
+				const unsigned int col = indexMap[vertex];
+				weight_matrix(currRow, col) = 1;
+			}
+		}
+		weight_matrix(currRow, currRow) = -rowSum;
+	}
+
+	// Transfer matrices to MatLab
+	int result = MatlabGMMDataExchange::SetEngineDenseMatrix("rhs", rhs);
+	result = MatlabGMMDataExchange::SetEngineSparseMatrix("weights", weight_matrix);
+
+	// Solve for the coordinates
+	MatlabInterface::GetEngine().Eval("weights = weights * -1");
+	MatlabInterface::GetEngine().Eval("rhs = rhs * -1");
+	MatlabInterface::GetEngine().Eval("coords = solve_linear_system_with_cholesky(weights, rhs)");
+
+	// Get the coords back from MatLab
+	GMMDenseColMatrix coord_matrix(rowCount, 2);
+	MatlabGMMDataExchange::GetEngineDenseMatrix("coords", coord_matrix);
+
+	// Set the coords to u, v
+	for (unsigned int currRow = 0; currRow < rowCount; ++currRow) {
+		const int index = rowMap[currRow];
+		const double uValue = coord_matrix(currRow, 0);
+		const double vValue = coord_matrix(currRow, 1);
+		u[index] = uValue;
+		v[index] = vValue;
+	}
+}
+
+// Creates (u,v) coordinates for all vertices using a cotangent-weight matrix,
+// Assuming the coordinates of the boundary are fixed and a row map is known
+void cotangentWeights(const MFnMesh& meshFn, MFloatArray& u, MFloatArray& v, 
+					const MIntArray& rowMap, std::map<int,int>& indexMap, 
+					unsigned int rowCount) {
+	GMMSparseRowMatrix weight_matrix(rowCount, rowCount);
+	GMMDenseColMatrix rhs(rowCount, 2);
+
+	MItMeshVertex vertex_it(meshFn.object());
+
+	// Initialize the matrix here, TODO
+
+	// Transfer matrices to MatLab
+	int result = MatlabGMMDataExchange::SetEngineDenseMatrix("rhs", rhs);
+	result = MatlabGMMDataExchange::SetEngineSparseMatrix("weights", weight_matrix);
+
+	// Solve for the coordinates
+	MatlabInterface::GetEngine().Eval("weights = weights * -1");
+	MatlabInterface::GetEngine().Eval("rhs = rhs * -1");
+	MatlabInterface::GetEngine().Eval("coords = solve_linear_system_with_cholesky(weights, rhs)");
+
+	// Get the coords back from MatLab
+	GMMDenseColMatrix coord_matrix(rowCount, 2);
+	MatlabGMMDataExchange::GetEngineDenseMatrix("coords", coord_matrix);
+
+	// Set the coords to u, v
+	for (unsigned int currRow = 0; currRow < rowCount; ++currRow) {
+		const int index = rowMap[currRow];
+		const double uValue = coord_matrix(currRow, 0);
+		const double vValue = coord_matrix(currRow, 1);
+		u[index] = uValue;
+		v[index] = vValue;
+	}
+}
+
+// Creates (u,v) coordinates for all vertices using a mean-value-weight matrix,
+// Assuming the coordinates of the boundary are fixed and a row map is known
+void meanValueWeights(const MFnMesh& meshFn, MFloatArray& u, MFloatArray& v, 
+					const MIntArray& rowMap, std::map<int,int>& indexMap, 
+					unsigned int rowCount) {
+	GMMSparseRowMatrix weight_matrix(rowCount, rowCount);
+	GMMDenseColMatrix rhs(rowCount, 2);
+
+	MItMeshVertex vertex_it(meshFn.object());
+
+	// Initialize the matrix here, TODO
+
+	// Transfer matrices to MatLab
+	int result = MatlabGMMDataExchange::SetEngineDenseMatrix("rhs", rhs);
+	result = MatlabGMMDataExchange::SetEngineSparseMatrix("weights", weight_matrix);
+
+	// Solve for the coordinates, using LU as the matrix isn't symmetric positive semi-definite
+	MatlabInterface::GetEngine().Eval("coords = solve_linear_system_with_LU(weights, rhs)");
+
+	// Get the coords back from MatLab
+	GMMDenseColMatrix coord_matrix(rowCount, 2);
+	MatlabGMMDataExchange::GetEngineDenseMatrix("coords", coord_matrix);
+
+	// Set the coords to u, v
+	for (unsigned int currRow = 0; currRow < rowCount; ++currRow) {
+		const int index = rowMap[currRow];
+		const double uValue = coord_matrix(currRow, 0);
+		const double vValue = coord_matrix(currRow, 1);
+		u[index] = uValue;
+		v[index] = vValue;
+	}
+}
+
+
+
+// Create a UV set with given name and set it properly
+void createUV(MFnMesh& meshFn, const MFloatArray& u, const MFloatArray& v, const char* name) {
+	const MString uvName = name;
+	meshFn.createUVSetWithName(uvName);
+	meshFn.setUVs(u, v, &uvName);
+	MIntArray uvCounts, uvIds;
+	meshFn.getVertices(uvCounts, uvIds);
+	meshFn.assignUVs(uvCounts, uvIds, &uvName);
+}
+
 harmonicFlatteningCmd::harmonicFlatteningCmd() = default;
 
 void* harmonicFlatteningCmd::creator() {
@@ -20,7 +216,6 @@ MString harmonicFlatteningCmd::commandName() {
 bool harmonicFlatteningCmd::isUndoable() const {
 	return false;
 }
-
 
 MStatus harmonicFlatteningCmd::doIt(const MArgList& argList) {
 	// Checks to see if everything is alright
@@ -82,126 +277,32 @@ MStatus harmonicFlatteningCmd::doIt(const MArgList& argList) {
 	u.setLength(meshFn.numVertices());
 	v.setLength(meshFn.numVertices());
 
-	std::list<int> boundary;
-	getBoundary(meshFn, boundary);
+	const unsigned int rowCount = calculateBoundaryCoords(meshFn, u, v);
 
-	// Get the differences between the coords of pairs of vertices on the boundary
-	std::list<MPoint> boundaryCoords = {};
-	MPoint coords;
-	for (int vertex : boundary) {
-		meshFn.getPoint(vertex, coords);
-		boundaryCoords.emplace_back(coords);
-	}
-	std::list<MVector> differences = {};
-	std::adjacent_difference(boundaryCoords.begin(), boundaryCoords.end(), 
-		std::back_inserter(differences));
+	// Create 3 sets of u,v arrays, copy to them the already-initialized
+	// boundary values. This should take quite some time
+	MFloatArray unif_u, unif_v, cot_u, cot_v, mean_u, mean_v;
+	unif_u.copy(u);
+	unif_v.copy(v);
+	cot_u.copy(u);
+	cot_v.copy(v);
+	mean_u.copy(u);
+	mean_v.copy(v);
 
-
-	// Sum the lengths of the differences to get the total boundary length
-	float totalBoundaryLength = 0;
-	auto i = differences.begin();
-	for (++i; i != differences.end(); ++i) {
-		totalBoundaryLength += (*i).length();
-	}
-
-	// Initialize iterators to go over the boundaries except the first and last
-	auto j = boundary.begin(), j_end = boundary.end();
-	auto i_end = differences.end();
-	--j_end;
-	--i_end;
-	i = differences.begin();
-
-	// Set the boundary coords, at last
-	u[*j] = 1;
-	v[*j] = 0;
-	float cumulativeBoundaryLength = 0; 
-	for (++i, ++j ; i != i_end && j != j_end; ++i, ++j) {
-		cumulativeBoundaryLength += (*i).length() / totalBoundaryLength;
-		u[*j] = cosf(2 * M_PI * cumulativeBoundaryLength);
-		v[*j] = sinf(2 * M_PI * cumulativeBoundaryLength);
-	}
-
-	int n = meshFn.numVertices(), m = boundary.size() - 1;
-	GMMSparseRowMatrix weight_matrix(n - m, n - m);
-	GMMDenseColMatrix rhs(n - m, 2);
-
-	MItMeshVertex vertex_it(meshFn.object());
-
-	// Initialize a map between rows in the matrix and indexes in the mesh,
-	// and the other way around
-	int row = 0;
-	MIntArray rowMap(n - m);
+	MIntArray rowMap(rowCount);
 	std::map<int, int> indexMap;
-	while (!vertex_it.isDone()) {
-		if (!vertex_it.onBoundary()) {
-			rowMap[row] = vertex_it.index();
-			indexMap[vertex_it.index()] = row;
-			++row;
-		}
-		vertex_it.next();
-	}
+	createMaps(meshFn, rowMap, indexMap, rowCount);
 
-	// Fill in the weight matrix and the rhs vector, for now with uniform weights
-	int col, _, rowSum;
-	MIntArray connectedVertices;
-	for (int currRow = 0; currRow < n - m; ++currRow) {
-		rowSum = 0;
-		vertex_it.setIndex(rowMap[currRow], _);
-		vertex_it.getConnectedVertices(connectedVertices);
-		for (const int vertex : connectedVertices) {
-			vertex_it.setIndex(vertex, _);
-			++rowSum;
+	// Create the 3 (u,v) sets
+	uniformWeights(meshFn, unif_u, unif_v, rowMap, indexMap, rowCount);
+	// cotangentWeights(meshFn, cot_u, cot_v, rowMap, indexMap, rowCount);
+	// meanValueWeights(meshFn, mean_u, mean_v, rowMap, indexMap, rowCount);
 
-			if (vertex_it.onBoundary()) {
-				rhs(currRow, 0) = - u[vertex];
-				rhs(currRow, 1) = - v[vertex];
-			} else {
-				col = indexMap[vertex];
-				weight_matrix(currRow, col) = 1;
-			}
-			if (false);
-		}
-		weight_matrix(currRow, currRow) = -rowSum;
-	}
+	// Assign the 3 created (u,v) mappings to the mesh
+	createUV(meshFn, unif_u, unif_v, "Uniform Weight Harmonic");
+	// createUV(meshFn, cot_u, cot_v, "Cotangent Weight Harmonic");
+	// createUV(meshFn, mean_u, mean_v, "Mean Value Weight Harmonic");
 
-	if (false);
-
-	// Transfer matrices to MatLab
-	int result = MatlabGMMDataExchange::SetEngineDenseMatrix("rhs", rhs);
-	result = MatlabGMMDataExchange::SetEngineSparseMatrix("weights", weight_matrix);
-
-	// Solve for the coordinates
-	MatlabInterface::GetEngine().Eval("weights = weights * -1");
-	MatlabInterface::GetEngine().Eval("rhs = rhs * -1");
-	MatlabInterface::GetEngine().Eval("coords = solve_linear_system_with_cholesky(weights, rhs)");
-
-	// Get the coords back from MatLab
-	GMMDenseColMatrix coord_matrix(n - m, 2);
-	MatlabGMMDataExchange::GetEngineDenseMatrix("coords", coord_matrix);
-
-	// Set the coords to u, v
-	int index;
-	for (int currRow = 0; currRow < n - m; ++currRow) {
-		index = rowMap[currRow];
-		double uValue = coord_matrix(currRow, 0);
-		double vValue = coord_matrix(currRow, 1);
-		u[index] = uValue;
-		v[index] = vValue;
-	}
-
-	for (int i = 0; i < n; i++) {
-		cout << u[i] << " " << v[i] << endl;
-	}
-
-	// Create the UV set and set it properly
-	MString uvName = "HarmonicUV";
-	meshFn.createUVSetWithName(uvName);
-	meshFn.setUVs(u, v, &uvName);
-	MIntArray uvCounts, uvIds;
-	meshFn.getVertices(uvCounts, uvIds);
-	meshFn.assignUVs(uvCounts, uvIds, &uvName);
-
-	return MS::kSuccess;
 }
 
 MSyntax harmonicFlatteningCmd::syntax() {
